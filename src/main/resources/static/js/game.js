89c7f1fc698a3f226f126eec581b60dcab053698
Erik Sillén <esillen@kth.se>
2020-11-21 21:58:58 +0100

    Moved toprow to own file. Username clickable.

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,7 +1,6 @@
 function sendChallengeCode(code) {
     console.log("sending challenge code: " + code);
     $.post( "code?day=" + activeDay, code, function( data ) {
-        console.log(window.alert("Yay du vann!!"));
+        //console.log(window.alert("Yay du vann!!"));
     });
-
 }
