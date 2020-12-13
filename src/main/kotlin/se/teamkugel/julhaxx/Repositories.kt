8a271b8628151234cee82bb983e459c02a39fba6
Daniel Sjöholm <daniel@b50.se>
2020-12-13 22:02:40 +0100

    Fixed #5 by pessimistic locking. Might have throttled our endpoint fantastically, dunno.

--- src/main/kotlin/se/teamkugel/julhaxx/Repositories.kt
+++ src/main/kotlin/se/teamkugel/julhaxx/Repositories.kt
@@ -1,8 +1,13 @@
 package se.teamkugel.julhaxx
 
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.repository.CrudRepository
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.LockModeType
 
 interface UserRepository : CrudRepository<JulhaxxUser, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Transactional
     fun findByUsername(username: String): JulhaxxUser?
 }
 
